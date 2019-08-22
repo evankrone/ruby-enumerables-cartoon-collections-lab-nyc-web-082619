05a094a75af8254e3585ca7cabde14cf86b46044
Evan Krone <evankrone1@gmail.com>
2019-08-22 00:24:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)
  dwarves = [ "Dopey", "Bashful", "Grumpy", "Doc", "Happy", "Sleepy", "Sneezy"]
    dwarves.each_with_index{ |element, index|
-   puts "#{index} .#{element}\n"
+   puts "#{index}.#{element}\n"
      }
    end
 
